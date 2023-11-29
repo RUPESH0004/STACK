32696c163aa1cfc940f59f7255de1a763f6d841e
RUPESH0004 <rj9082356865@gmail.com>
2023-11-29 18:05:33 +0530

    start programinfg

---
+++ implimantionofstack.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+class stack {
+    int data ;
+    int *arr;
+    int top ;
+    
+    public:
+    stack (int data ){
+        this ->data = data;
+
+    }
+
+
+};
+int main (){
+     
+}
