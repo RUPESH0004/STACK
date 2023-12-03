259f519b49b38fdc6d7bdfe6bfece88a86c126e8
RUPESH0004 <rj9082356865@gmail.com>
2023-12-03 23:11:11 +0530

    Revert "traying to impliment stack by my self"
    This reverts commit 892dacfe3d8bfddbde6d11be4c6bd583258799a3.

--- implimantionofstack.cpp
+++ implimantionofstack.cpp
@@ -1,53 +1,19 @@
-#include <iostream>
+#include<iostream>
+#include<stdlib.h>
 using namespace std;
-int *arry;
-int top;
-int size;
-class stack
-{
-public:
-    stack(int size)
-    {
-        arry[size];
-        top = -1;
+class stack {
+    int data ;
+    int *arr;
+    int top ;
+    
+    public:
+    stack (int data ){
+        this ->data = data;
+
     }
+
+
 };
-void push(int data)
-{
-    if (top == -1)
-    {
-        top = 0;
-        arry[top] = data;
-        cout << arry[top];
-    }
-    else if (top == size - 1)
-    {
-        cout << "the given arry is over flow" << endl;
-    }
-    else
-    {
-        top++;
-        arry[top] = data;
-        cout << arry[top];
-    }
-}
-int display()
-{
-    if (top >= 0)
-    {
-        for (int i = 0; i <= top; i++)
-        {
-            cout << arry[i];
-        }
-    }
-    else
-    {
-        cout << "the array is under flow" << endl;
-    }
-}
-int main()
-{
-    stack st(5);
-    st.push(45);
-    st.display();
+int main (){
+     
 }
